832753d7cfb4a24731b25749a5dcb31101ac4a4b
DarijaKR <darija.knezevic@hotmail.com>
2024-11-07 17:22:00 +0100

    Reverting commented code

--- rm.py
+++ rm.py
@@ -235,9 +235,9 @@ try:
 
     if len(xInput) == len(yInput):
       calculateDualModel(xInput, yInput)
-      # calculateQuadraticModel(xInput, yInput)
-      # getBestSlicedModel(xInput, yInput, False)
-      # getBestSlicedModel(xInput, yInput, True)
+      calculateQuadraticModel(xInput, yInput)
+      getBestSlicedModel(xInput, yInput, False)
+      getBestSlicedModel(xInput, yInput, True)
     else:
       print('X and Y values are not the same length')
 except Exception as err:
